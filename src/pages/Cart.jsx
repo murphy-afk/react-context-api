95fa1a92827e82b484aa07af181bfea17b5a607f
murphy-afk <bardarenoemi@gmail.com>
2025-12-11 17:25:00 +0100

    feat: cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,21 @@
-export default function Cart() {
+import { useCollections } from "../context/CollectionsContext"
+
+export default function Favourites() {
+  const { cart } = useCollections();
+  console.log(cart);
+
   return (
-    null
-  )
+    <>
+      <h1 className="text-center pt-0 pb-2 bg-secondary w-100">Your Cart</h1>
+      {cart.length === 0 ? (
+        <div className="container text-center bg-semitransparent py-2 text-light">
+          <p>Your cart is empty</p>
+        </div>) :
+        (
+          <div>
+            <h1>There are {cart.length} products in your cart</h1>
+          </div>
+        )}
+    </>
+  );
 }
